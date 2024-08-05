49f1b6cc31449ff529df2a7ffcefb585797781bb
Adrien <amaugueret@gmail.com>
2024-08-06 00:28:39 +0200

    Added base GameController and createplayer factory function

--- script.js
+++ script.js
@@ -29,3 +29,54 @@ const GameBoard = (function () {
   };
 })();
 
+function createPlayer (name, mark) {
+  return { name, mark }
+}
+
+function GameController (player1="p1", player2="p2") {
+  const board = GameBoard
+
+  const p1 = createPlayer(player1, "X")
+  const p2 = createPlayer(player2, "O")
+
+  let activePlayer = p1
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === p1 ? p2 : p1;
+  };
+
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const playRound = (pos) => {
+    console.log(
+      `${getActivePlayer().name} played in cell ${pos}`
+    );
+    if (board.checkCell(pos)) {
+      board.updateBoard(pos, getActivePlayer().mark);
+      /*  This is where we would check for a winner and handle that logic,
+        such as a win message. */
+
+      // Switch player turn
+      switchPlayerTurn();
+    }
+    else {
+      console.log(`Invalid move for ${activePlayer.mark}. Try again.`)
+    }
+    
+    printNewRound();
+  };
+
+  printNewRound();
+
+  return {
+    playRound,
+    getActivePlayer
+  };
+}
+
+const game = GameController();
